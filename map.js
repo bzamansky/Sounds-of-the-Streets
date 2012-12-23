14bc79e57653737ae94c29ac361f670bdb61e672
Batya Zamansky <batya@rufferto.(none)>
2012-12-23 13:12:01 -0500

    hey, i have working stuff! Okay, you can click on a place to make a marker and then if you click on the marker it gives an address

--- map.js
+++ map.js
@@ -1,7 +1,9 @@
 var map;
-var marker;
+var geocoder;
+var infowindow;
 
 function initialize() {
+    geocoder = new google.maps.Geocoder();
     var mapOptions = {
         center: new google.maps.LatLng(40.713956,-74.377441),
         zoom: 8,
@@ -12,35 +14,37 @@ function initialize() {
     google.maps.event.addListener(map, 'click', function(event) {
         placeMarker(event.latLng);
     });
+
 }
 
 function placeMarker(location) {
-    marker = new google.maps.Marker({
+    var marker = new google.maps.Marker({
         position: location,
         map: map
     });
-    var infowindow = new google.maps.InfoWindow(
+    infowindow = new google.maps.InfoWindow(
 	{ content: "" +marker.position,
           size: new google.maps.Size(50,50)
 	});
     google.maps.event.addListener(marker, 'click', function() {
-	codeLatLng();
-	infowindow.open(map,marker);
+	codeLatLng(marker);
+//	infowindow.open(map,marker);
     });
     
 }
 
-function codeLatLng() {
-    var input = document.getElementById("latlng").value;
-    var latlngStr = input.split(",");
-    var lat = parseFloat(latlngStr[0]);
-    var lng = parseFloat(latlngStr[1]);
+function codeLatLng(marker) {
+    console.log( marker.position["Ya"]);
+    console.log(marker.position["Za"]);
+//    var input = marker.position;
+//    var latlngStr = input.split(",");
+    var lat = marker.position["Ya"];
+    var lng = marker.position["Za"];
     var latlng = new google.maps.LatLng(lat, lng);
     geocoder.geocode({'latLng': latlng}, function(results, status) {
 	if (status == google.maps.GeocoderStatus.OK) {
-	    console.log(results);
           if (results[1]) {
-              console.log(results);
+              console.log(results[1]);
               infowindow.setContent(results[1].formatted_address);
               infowindow.open(map, marker);
           }
