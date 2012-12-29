2faf61d611d66204cd111fcfee7d9ad9b5cdcf4a
Batya Zamansky <batya@rufferto.(none)>
2012-12-29 10:49:26 -0500

    made a method to get the url of a searched track through the api

---
+++ utils.py
@@ -0,0 +1,18 @@
+import json, urllib2
+
+key = "0946dbffa1b5b7ad9c4dc855be73398f"
+
+def getSong(k,name):
+    title = name.split()
+    track = ""
+    for item in title:
+        track = track + item + "+"
+    track = track[:-1]
+    
+    url = "http://ws.audioscrobbler.com/2.0/?method=track.search&track=%s&api_key=%s&format=json"%(track,k)
+    request = urllib2.urlopen(url)
+    result = json.loads(request.read())
+    return result["results"]["trackmatches"]["track"][0]["url"]
+
+
+print getSong(key, "it will rain")
