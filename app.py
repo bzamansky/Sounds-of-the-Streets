8e9687aa5da87a5670dd1e55aa4bc15159921754
Batya Zamansky <batya@rufferto.(none)>
2012-12-29 10:30:32 -0500

    made more like a web app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+app.secret_key="blah"
+key= "0946dbffa1b5b7ad9c4dc855be73398f"
+
+@app.route("/")
+def home():
+    return render_template("map.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 5000)
---
+++ static/map.js
@@ -0,0 +1,86 @@
+var map;
+var geocoder;
+var infowindow;
+var markersArray = [];
+var selected;
+
+
+function initialize() {
+//Initializes the map.
+
+    geocoder = new google.maps.Geocoder();
+    var mapOptions = {
+        center: new google.maps.LatLng(40.713956,-74.377441),
+        zoom: 11,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("map_canvas"),
+				  mapOptions);
+    google.maps.event.addListener(map, 'click', function(event) {
+        placeMarker(event.latLng);
+    });
+
+}
+
+function placeMarker(location) {
+//This allows you to print a marker.
+
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+    selected = marker;
+    markersArray.push(marker);
+    infowindow = new google.maps.InfoWindow(
+	{ content: "" +marker.position,
+          size: new google.maps.Size(50,50)
+	});
+    google.maps.event.addListener(marker, 'click', function(){
+	selected = marker;
+	infowindow.open(map,marker);
+	codeLatLng(marker);
+    });
+    
+}
+
+function removeMarkers(){
+    if(markersArray){
+	for (i in markersArray){
+	    markersArray[i].setMap(null);
+	}
+    }
+}
+
+function removeMark(){
+    selected.setMap(null);
+}
+
+function codeLatLng(marker) {
+//This allows you to click on a marker and return it's address.
+
+    var lat = marker.position["Ya"];
+    var lng = marker.position["Za"];
+    var latlng = new google.maps.LatLng(lat, lng);
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+	if (status == google.maps.GeocoderStatus.OK) {
+          if (results[0]) {
+              infowindow.setContent(results[0].formatted_address);
+              infowindow.open(map, marker);
+	      return results[0].formatted_address;
+          }
+	}
+       else {
+          alert("Geocoder failed due to: " + status);
+      }
+    });
+
+    return undefined;
+}
+
+
+
+
+$(document).ready(function(){
+    $("#clear").click(removeMarkers);
+    $("#remove").click(removeMark);
+});
