7de13c5b5b9031d20457d129e4858c2bd1bb8f09
Batya Zamansky <batya@rufferto.(none)>
2012-12-31 16:16:33 -0500

    trying to make everything work through python flask-y stuff

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import utils
+import map_utils
 from flask import Flask
 from flask import render_template
 
 
 app = Flask(__name__)
 app.secret_key="blah"
-key= "0946dbffa1b5b7ad9c4dc855be73398f"
+Last_key= "0946dbffa1b5b7ad9c4dc855be73398f"
+Map_key = "AIzaSyDm3LFbtgPrB8jtcruyGlf9ED-tidYvYrA"
 
 @app.route("/")
 def home():
+    s = map_utils.source();
     address = "line goes here"
-    return render_template("map.html", address=address)
+    return render_template("map.html", address=address, s=s)
 
 
 
---
+++ map_utils.py
@@ -0,0 +1,8 @@
+from googlemaps import GoogleMaps
+
+key = "AIzaSyDm3LFbtgPrB8jtcruyGlf9ED-tidYvYrA"
+gmaps = GoogleMaps(key)
+
+
+def source():
+    return "https://maps.googleapis.com/maps/api/js?key=AIzaSyDm3LFbtgPrB8jtcruyGlf9ED-tidYvYrA&sensor=true"
