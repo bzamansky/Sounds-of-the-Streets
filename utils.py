14a4193fbb7be51797f3de18a9191297365b9f92
Batya Zamansky <batya@rufferto.(none)>
2013-01-12 18:38:37 -0500

    to be honest i'm not sure what i'm doing.  i think i'm trying to make the youtube thing work but i don't know anymore

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import json, urllib2
+import gdata.youtube
+import gdata.youtube.service
 
 key = "0946dbffa1b5b7ad9c4dc855be73398f"
 
@@ -50,4 +52,48 @@ def getSong2(k,names):
         except:
             x = "http://www.last.fm/music/Rick+Astley/_/Never+Gonna+Give+You+Up?ac=never+gonna+give+you+"
     return x
+
+
+###############
+#Youtube Stuff#
+###############
+
+def initialize():
+
+    yt_service = gdata.youtube.service.YouTubeService()
+
+    # Turn on HTTPS/SSL access.
+    # Note: SSL is not available at this time for uploads.
+    yt_service.ssl = True
+
+    yt_service.developer_key = "AI39si5ftQAadvFWghwUFyQgZVRXIj_E-J8bmlv1PeQCZ5dQQneayRz1XV822s8phGDKpqknrl7n0HoiSLwaxfd5HlbU4s2iMg"
+    yt_service.client_id = key
+
+def getTopVideo( searchTerm):
+    yt_service = gdata.youtube.service.YouTubeService()
+    query = gdata.youtube.service.YouTubeVideoQuery()
+    query.vq = searchTerm
+    query.orderby = 'relevance'
+    query.racy = 'include'
+    feed = yt_service.YouTubeQuery(query)
+    return feed.entry[0]
+
+initialize()
+print getTopVideo("tonight")
+
+"""
+def getVideo(k,name):
+    song = name.split()
+    track = ""
+    for item in song:
+        track = track + item + '%20'
+    track = track[:-1]
+    track = urllib2.quote(track)
+    url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=%s&key=%s"%(track,k)
+    request = urllib2.urlopen(url)
+    result = json.loads(request.read())
+    print result
+"""
+#getVideo(key, "believe")
+
 
